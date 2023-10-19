3152e6aac1dcda7f965f727cfb0d38729ea3d8dd
Btogrul <baghirli.togrul@gmail.com>
2023-10-19 04:27:46 +0400

    new updates

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,14 @@
 public class Car {
+    String make;
+    String model;
+    int year;
+    int mil;
+
+    public double milCalculate(){
+       return this.mil * 1.6;
+    }
+
+    public int carAge(){
+        return 2023 - this.year;
+    }
 }
---
+++ src/CarMain.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.print("Markani daxil edin ");
+        car.make = sc.nextLine();
+        System.out.println("-----------");
+
+        System.out.print("modeli daxil edin ");
+        car.model = sc.nextLine();
+        System.out.println("-----------");
+
+        System.out.println();
+
+
+
+
+
+
+        System.out.print("Mashinin ilini daxil edin :");
+
+        car.year = sc.nextInt();
+        if (car.year != 0 && car.year >  1886 && car.make != "" && car.model != ""){
+            System.out.print("Mashinin Milini daxil edin :");
+            car.mil = sc.nextInt();
+            if (car.mil >=0){
+                System.out.println(" Nətircə olaraq: ");
+                System.out.println(" Markasi: " + car.make);
+                System.out.println(" Modeli: " + car.model);
+                System.out.println(" Yashi: " + car.carAge());
+                System.out.println(" Toplam km: " + car.milCalculate());
+
+            }else {
+                System.out.println("xeta");
+            }
+        }else {
+            System.out.println(" Melumatlari tam doldurun ");
+        }
+
+    }
+}
