974c02beb67524a6bc9ed3ec544d405ad151588f
Btogrul <baghirli.togrul@gmail.com>
2023-10-28 11:08:38 +0400

    new updates

---
+++ src/Archive/mentor/M6/Rectangle.java
@@ -0,0 +1,10 @@
+package Archive.mentor.M6;
+
+public class Rectangle {
+    public int RectangleArea(int a, int b) {
+        return a * b;
+    }
+    public int RectanglePerimetr(int a, int b){
+        return  2 * (a + b);
+    }
+}
