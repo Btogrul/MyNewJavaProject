46b181de954cca90482d0666df4ac89a1ee8efa6
Btogrul <baghirli.togrul@gmail.com>
2023-12-04 15:33:59 +0400

    last

---
+++ src/Archive/mentor/Month_3/M20/TaskString.java
@@ -0,0 +1,31 @@
+package Archive.mentor.Month_3.M20;
+
+import java.util.Scanner;
+
+public class TaskString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("type something... ");
+        String user_input = sc.nextLine();;
+
+        StringBuilder my_build;
+        my_build = new StringBuilder(user_input);
+
+        System.out.println("my string output -->>> " + user_input.replaceAll("[aeiouAEIOU]", ""));
+
+
+        for (int i = 0; i < my_build.length(); ) {
+            char currentChar = my_build.charAt(i);
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u'
+                    || currentChar == 'A' || currentChar == 'E' || currentChar == 'I' || currentChar == 'O' || currentChar == 'U') {
+                my_build.deleteCharAt(i);
+            } else {
+                i++;
+            }
+        }
+
+        System.out.println("my StringBuilder output -->>> " + user_input.replaceAll("[aeiouAEIOU]", ""));
+
+    }
+}
