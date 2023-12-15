4d6b264021c81f3f2f40add6c46c13e69f8442f5
Btogrul <baghirli.togrul@gmail.com>
2023-12-16 00:17:47 +0400

    last update

--- src/Archive/mentor/Month_3/M23/Calculate.java
+++ src/Archive/mentor/Month_3/M23/Calculate.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Calculate {
     double calculateTotal(Map<String, Double> items) {
-        double totalPrice = 0.0;
+        double totalPrice =0;
         for (double price : items.values()) {
             totalPrice += price;
         }
